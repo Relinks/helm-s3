614caddd8fb5bfefb9d32a0412edef3cbaddcb4d
Jeroen Jans <jeroen.jans@rorix.nl>
2020-01-17 11:35:26 +0100

    Modifications for a windows build

--- hack/build.sh
+++ hack/build.sh
@@ -3,18 +3,17 @@
 # This emulates GOPATH presence for go tool.
 # This is need because helm installs plugins into ~/.helm/plugins.
 
-projectRoot="$1"
-pkg="$2"
-
-if [ ! -e "${GOPATH}/src/${pkg}" ]; then
-    mkdir -p $(dirname "${GOPATH}/src/${pkg}")
-    ln -sfn "${projectRoot}" "${GOPATH}/src/${pkg}"
-fi
-
 version="${HELM_S3_PLUGIN_VERSION:-}"
 if [ -z "${version}" ]; then
   version="$(cat plugin.yaml | grep "version" | cut -d '"' -f 2)"
 fi
 
-cd "${GOPATH}/src/${pkg}"
+# Build the linux release
 go build -o bin/helms3 -ldflags "-X main.version=${version}" ./cmd/helms3
+
+# Build the windows release
+GOOS=windows GOARCH=amd64 go build -o bin/helms3.exe -ldflags "-X main.version=${version}" ./cmd/helms3
+
+# Package the releases
+tar -czf s3-windows.tar.gz plugin.yaml bin/helms3.exe LICENSE
+tar -czf s3.tar.gz plugin.yaml bin/helms3 LICENSE
